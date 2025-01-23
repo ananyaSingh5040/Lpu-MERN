c3f2cf98b436dea9b79148a1d57f5c4746177ec7
Ananya Singh <singhananya5040@gmail.com>
2025-01-23 20:31:00 +0530

    day-7

---
+++ JS/day-7/script.js
@@ -0,0 +1,109 @@
+// console.log("Hello World")
+
+//var a = "GTK";
+//console.log(a)
+
+//console.log(b); //OP: undefined
+//var b = "SK";
+
+//console.log(states);
+
+//let city = "GTK";
+//console.log(city);
+
+//console.log(state);
+//let state = "SIKKIM";
+
+//let city = 123;
+
+//const
+// const city = "delhi";
+// console.log(city);
+// console.log(state);
+// const state = "Sikkim"
+
+//const city = "punjab";
+
+// var city = "Delhi";
+// console.log(city);
+
+// var city = "Mumbai";
+// console.log(city);
+
+// let city = "Delhi"; // city = "Delhi"
+// console.log(city); // Delhi
+
+// console.log(country);
+// let country = "India"; // country = "India"
+// // let country === DECLARATION part
+// // = 'India' === INITIALIZATION part
+
+// // let city = 123;
+
+// const city = "Delhi"; 
+// console.log(city);
+
+// const country = "India";
+// console.log(country);
+
+// city = "Mumbai"; 
+// console.log(city);
+
+// let city = "delhi";
+// let pinCode = 441122; 
+// let isUserDefined = false;
+// let data = {}; 
+// let fruits = []; 
+
+// const cty = 'Delhi'
+// console.log(city);
+// city = {};
+// console.log(city);
+
+// const city = "Delhi";
+// const country = "India";
+
+// const address = city + ", " + country;
+// console.log(address);
+
+// const ans1 = 10 + 30;
+// console.log(ans1); // --> 40
+
+// const ans2 = 10 + "Hello";
+// console.log(ans2); // --> 10Hello
+
+// const ans3 = "Hello" + 10;
+// console.log(ans3); // --> Hello10
+
+// const ans4 = "Hello" + " All!";
+// console.log(ans4); // --> Hello All!
+
+// const ans5 = "10" + "20";
+// console.log(ans5); // 1020
+
+// const ans6 = 10 + "20";
+// console.log(ans6); // 1020
+
+// const ans7 = 10 + 20;
+// console.log(ans7); // 1020
+
+const ans1 = 30 - 10;
+console.log(ans1); // --> 20
+
+const ans2 = 10 - "Hello";
+console.log(ans2); // --> NaN
+
+const ans3 = "Hello" - 10;
+console.log(ans3); // --> NaN
+
+const ans4 = "Hello" - " All!";
+console.log(ans4); // --> NaN
+
+const ans5 = "90" - "10"; // --> 90 - 10
+console.log(ans5); // 80
+
+const ans6 = 20 - "10"; // --> 20 - 10
+console.log(ans6); // 10
+
+const ans7 = 50 - 20;
+console.log(ans7); // 30
