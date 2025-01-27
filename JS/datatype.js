f19b7aab5737c67a6d2c0e1aa55281673a0e6f20
Ananya Singh <singhananya5040@gmail.com>
2025-01-27 09:54:52 +0530

    27-01-25

---
+++ JS/datatype.js
@@ -0,0 +1,17 @@
+let a= 40;  //integer
+let b= true; //boolean
+let c= "ANANYA SINGH"; //string
+let d= undefined; //no assigned value
+let e= null; //a value is assigned but is null
+let f= { //object
+    name: "ananya singh",
+    age: 20
+}
+let g= Symbol("hello");
+let h=Symbol("hello");
+//Symbol is used for unique data.
+
+console.log(g==h);
+//logs false on the console.
+"use strict" 
+//Strict mode.
