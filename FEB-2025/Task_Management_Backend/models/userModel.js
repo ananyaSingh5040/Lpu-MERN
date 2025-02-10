12b795b6ea9e52bbd1271f711c0316524295e43d
Ananya Singh <singhananya5040@gmail.com>
2025-02-10 11:58:28 +0530

    10-02-25

---
+++ FEB-2025/Task_Management_Backend/models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose"); //mongoose is our helper which will talk in mongodb
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    trim: true, //trim hata deta extra space.
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+},{
+    timestamps: true,
+
+});
+//schema is kind of a constructor for mongoose
+const User= mongoose.model("users",userSchema);
+//collection name is upto you par keep it noun and plural
+module.exports= User;
