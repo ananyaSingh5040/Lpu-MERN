3fe670053e246b05822afa2cf08bbb499cf14627
Ananya Singh <singhananya5040@gmail.com>
2025-02-20 10:54:32 +0530

    20-02-25

--- FEB-2025/Task_Management_Frontend/src/pages/HomePage.jsx
+++ FEB-2025/Task_Management_Frontend/src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
-
+import "./home.css";
+import Sidebar from '../components/Sidebar';
 
 const HomePage = ({currUser,handleLogout}) => {
   return (
    <>
-   <Navbar currUser={currUser} handleLogout={handleLogout}/>
-   <h1>homepage</h1>
+   {/* <Navbar currUser={currUser} handleLogout={handleLogout}/>
+   <h1>homepage</h1> */}
+   <div className="flex">
+
+   <div className="sidebar"><Sidebar/></div>
+   <div className="workspace"></div>
+   </div>
    </>
   )
 }
 
-export default HomePage
+export default HomePage 
